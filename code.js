b68164e3d698e1630edfc988b1cb91d2833343c8
JollyJoker90 <eike@LAPTOP-22I97KGM.>
2024-11-27 07:26:41 +0100

    created basic math functions created operate function to use the math functions created input buttons

---
+++ code.js
@@ -0,0 +1,27 @@
+let num1;
+let num2;
+let operator;
+
+const add = (a, b) => {
+    return a + b;
+}
+
+const subtract = (a, b) => {
+    return a - b;
+}
+
+const multiply = (a, b) => {
+    return a * b;
+}
+
+const divide = (a, b) => {
+    return a / b;
+}
+
+const operate = (operator, num1, num2) => {
+    if (operator === "+") { return add(num1, num2) }
+    if (operator === "-") { return subtract(num1, num2) }
+    if (operator === "*") { return multiply(num1, num2) }
+    if (operator === "/") { return divide(num1, num2) }
+
+}
