25deba9a65110e176a1cc34c65cd281b089f01f6
JollyJoker90 <61844405+JollyJoker90@users.noreply.github.com>
2024-11-30 08:35:23 +0100

    changed math functions to take string and perform math operations used local variables within operate and the global variable operators

--- code.js
+++ code.js
@@ -2,33 +2,41 @@ const display = document.querySelector(".display");
 const numBtns = document.querySelectorAll(".num");
 const operatorBtns = document.querySelectorAll(".operator");
 
-let num1;
+let num1 = 0;
 let num2;
 let operator;
 
-
-
 const add = (a, b) => {
-    return a + b;
-}
+  return (parseInt(a) + parseInt(b)).toString();
+};
 
 const subtract = (a, b) => {
-    return a - b;
-}
+  return (parseInt(a) - parseInt(b)).toString();
+};
 
 const multiply = (a, b) => {
-    return a * b;
-}
+  return (parseInt(a) * parseInt(b)).toString();
+};
 
 const divide = (a, b) => {
-    return a / b;
-}
+  return (parseInt(a) / parseInt(b)).toString();
+};
 
-const operate = (operator, num1, num2) => {
-    if (operator === "+") { return add(num1, num2) }
-    if (operator === "-") { return subtract(num1, num2) }
-    if (operator === "*") { return multiply(num1, num2) }
-    if (operator === "/") { return divide(num1, num2) }
+const operate = (number1, number2) => {
+  if (operator === "+") {
+    return add(number1, number2);
+  }
+  if (operator === "-") {
+    return subtract(number1, number2);
+  }
+  if (operator === "*") {
+    return multiply(number1, number2);
+  }
+  if (operator === "/") {
+    return divide(number1, number2);
+  }
+};
 
-}
-
+const updateDisplay = (str) => {
+  display.textContent = str;
+};
