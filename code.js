47fa77ceebb85a32e7838b488ef0558a42ea1f7b
JollyJoker90 <61844405+JollyJoker90@users.noreply.github.com>
2024-12-04 21:53:42 +0100

    finished calculate function

--- code.js
+++ code.js
@@ -1,11 +1,10 @@
 const display = document.querySelector(".display");
-const numBtns = document.querySelectorAll(".num");
-const operatorBtns = document.querySelectorAll(".operator");
+// const numBtns = document.querySelectorAll(".num");
+// const operatorBtns = document.querySelectorAll(".operator");
 const btns = document.querySelectorAll("button");
 
 let previousNumber = "";
 let currentNumber = "";
-// let input;
 let operator = null;
 
 const appendNumber = (number) => {
@@ -18,74 +17,58 @@ btns.forEach((btn) => {
       appendNumber(btn.value);
     } else if (btn.classList.contains("operator")) {
       chooseOperator(btn.value);
-      // operator = btn.value;
-      // calculate();
     } else if (btn.classList.contains("equal")) {
       calculate();
+    } else if (btn.classList.contains("clear")) {
+      clear();
     }
     updateDisplay();
   });
 });
 
 const chooseOperator = (operation) => {
-  // if (currentNumber === "") return;
+  if (currentNumber === "") return;
+  if (previousNumber !== "") {
+    calculate();
+  }
   operator = operation;
   previousNumber = currentNumber;
   currentNumber = "";
 };
 
 const calculate = () => {
+  if (currentNumber === "") return;
   let prev = parseInt(previousNumber);
   let current = parseInt(currentNumber);
   let result;
 
   switch (operator) {
     case "+":
-      result = (num1 + num2).toString();
+      result = prev + current;
       break;
-
+    case "-":
+      result = prev - current;
+      break;
+    case "*":
+      result = prev * current;
+      break;
+    case "/":
+      result = current === 0 ? "ERROR" : prev / current;
+      break;
     default:
       break;
   }
+  currentNumber = result.toString();
+  operator = null;
+  previousNumber = "";
 };
 
-// numBtns.forEach((btn) => {
-//   btn.addEventListener("click", () => {
-//     input += btn.value;
-//     updateDisplay(input);
-//   });
-// });
-
-// If equal, calc num1, num2 with stored operator
-
-// If operator pressed, store input as num1
-//
-
-// operatorBtns.forEach((btn) => {
-//   btn.addEventListener("click", () => {
-//     if (btn.value === "=") {
-//       num2 = input;
-//       // operate(num1, num2 === "" ? "0" : num2)
-//       operate(num1, num2);
-//       updateDisplay(num1);
-//       input = "";
-//     } else {
-//       if (num1 === "") {
-//         num1 = input
-//         input = "";
-//       } else {
-//         num2 = input;
-//         operate(num1, num2);
-//         updateDisplay(num1);
-//         input = "";
-//       }
-//       operator = btn.value;
-//     }
-//   });
-// });
+const clear = () => {
+  currentNumber = "";
+  previousNumber = "";
+  operator = null;
+};
 
 const updateDisplay = () => {
   display.textContent = currentNumber || "0";
 };
-
-// init();
