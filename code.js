70753bfcc2312684910db90d155ebac60081334f
JollyJoker90 <61844405+JollyJoker90@users.noreply.github.com>
2024-12-04 19:41:24 +0100

    fixed displaying the current number added funtionality to use operator buttons in a light form. Not for math purpose but for general flow of input

--- code.js
+++ code.js
@@ -10,55 +10,44 @@ let operator = null;
 
 const appendNumber = (number) => {
   currentNumber += number;
-}
-
-const add = (a, b) => {
-  return (parseInt(a) + parseInt(b)).toString();
-};
-
-const subtract = (a, b) => {
-  return (parseInt(a) - parseInt(b)).toString();
-};
-
-const multiply = (a, b) => {
-  return (parseInt(a) * parseInt(b)).toString();
-};
-
-const divide = (a, b) => {
-  return (parseInt(a) / parseInt(b)).toString();
-};
-
-const operate = (number1, number2) => {
-  if (operator === "+") {
-    num1 = add(number1, number2);
-    // updateDisplay(num1);
-    // input = "";
-  }
-  if (operator === "-") {
-    num1 = subtract(number1, number2);
-    // updateDisplay(num1);
-    // input = "";
-  }
-  if (operator === "*") {
-    num1 = multiply(number1, number2);
-    // updateDisplay(num1);
-    // input = "";
-  }
-  if (operator === "/") {
-    num1 = divide(number1, number2);
-    // updateDisplay(num1);
-    // input = "";
-  }
 };
 
 btns.forEach((btn) => {
   btn.addEventListener("click", () => {
-    if (btn.classList.contains(".num")) {
+    if (btn.classList.contains("num")) {
       appendNumber(btn.value);
+    } else if (btn.classList.contains("operator")) {
+      chooseOperator(btn.value);
+      // operator = btn.value;
+      // calculate();
+    } else if (btn.classList.contains("equal")) {
+      calculate();
     }
     updateDisplay();
-  })
-})
+  });
+});
+
+const chooseOperator = (operation) => {
+  // if (currentNumber === "") return;
+  operator = operation;
+  previousNumber = currentNumber;
+  currentNumber = "";
+};
+
+const calculate = () => {
+  let prev = parseInt(previousNumber);
+  let current = parseInt(currentNumber);
+  let result;
+
+  switch (operator) {
+    case "+":
+      result = (num1 + num2).toString();
+      break;
+
+    default:
+      break;
+  }
+};
 
 // numBtns.forEach((btn) => {
 //   btn.addEventListener("click", () => {
@@ -70,31 +59,30 @@ btns.forEach((btn) => {
 // If equal, calc num1, num2 with stored operator
 
 // If operator pressed, store input as num1
-// 
+//
 
-
-operatorBtns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    if (btn.value === "=") {
-      num2 = input;
-      // operate(num1, num2 === "" ? "0" : num2)
-      operate(num1, num2);
-      updateDisplay(num1);
-      input = "";
-    } else {
-      if (num1 === "") {
-        num1 = input
-        input = "";
-      } else {
-        num2 = input;
-        operate(num1, num2);
-        updateDisplay(num1);
-        input = "";
-      }
-      operator = btn.value;
-    }
-  });
-});
+// operatorBtns.forEach((btn) => {
+//   btn.addEventListener("click", () => {
+//     if (btn.value === "=") {
+//       num2 = input;
+//       // operate(num1, num2 === "" ? "0" : num2)
+//       operate(num1, num2);
+//       updateDisplay(num1);
+//       input = "";
+//     } else {
+//       if (num1 === "") {
+//         num1 = input
+//         input = "";
+//       } else {
+//         num2 = input;
+//         operate(num1, num2);
+//         updateDisplay(num1);
+//         input = "";
+//       }
+//       operator = btn.value;
+//     }
+//   });
+// });
 
 const updateDisplay = () => {
   display.textContent = currentNumber || "0";
